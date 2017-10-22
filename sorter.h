2bddc20fe3e6b4e0176aacd07ce5f48b82579605
Steve Hsu <triangular.pyramid@gmail.com>
2017-10-22 11:43:35 -0400

    header file

---
+++ sorter.h
@@ -0,0 +1,9 @@
+#ifndef SORTER_H
+#define SORTER_H
+/*
+ * sorts the file with name "infile"
+ * by the fields listed in "fieldlist"
+ * and outputs the result in the directory "dirname"
+ */
+void sort_csv(char *infile, char *fieldlist, char *dirname);
+#endif
