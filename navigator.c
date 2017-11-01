2364597078826c77abd0e77d4a1a8ff3fa741cbb
Boyang Fu <bf249@ls.cs.rutgers.edu>
2017-10-31 20:07:43 -0400

    finish extra credit

--- navigator.c
+++ navigator.c
@@ -5,158 +5,205 @@
 #include<getopt.h>
 #include<unistd.h>
 #include"sorter.h"
+#include<libgen.h>
 
 
-	
 /*
- * navigation function serves as the function to recursively traverse the directroies, 
- * first argument name is the start directory from which we begin to search. The function
- * first traverse the content in the current directory, if not a directory, check the 
- * validity of the csv file format and the number of field name inside of the file, otherwise
- * go to the next directory and do the same procedure again.
- * if no sub directory in the current directory, stop fork.
- */
+* navigation function serves as the function to recursively traverse the directroies,
+* first argument name is the start directory from which we begin to search. The function
+* first traverse the content in the current directory, if not a directory, check the
+* validity of the csv file format and the number of field name inside of the file, otherwise
+* go to the next directory and do the same procedure again.
+* if no sub directory in the current directory, stop fork.
+*/
 
 
-int navigation(char *name, char *output_path, FILE *ptr){
+int navigation(char *name, char *output_path, FILE *ptr, int indent, char* dir_name) {
 	DIR *dir;
 	pid_t t_pid[256];
+	int dir_index[256];
+	
 	struct dirent *entry;
 	int status;
-	int process_num =0;
+	int process_num = 0;
 	int counter = 0;
-	pid_t pid = 0; 
+	pid_t pid = 0;
 	pid_t ppid = 0;
 
 
-	int i ;
-	for (i = 0; i < 32; i++){
+	int i;
+	for (i = 0; i < 256; i++) {
 		t_pid[i] = 0;
+		dir_index[i ] = 0;
 	}
+	char *filelist[256];
 
-		
-	
-	
-	if( !(dir = opendir(name) )){
+
+
+	if (!(dir = opendir(name))) {
 		printf("\ndirectory name is %s\n", name);
 		fprintf(stderr, "no such directory exist\n");
 		exit(0);
-	}		
-	else{
-		
+	}
+	else {
 
-		while( entry = readdir(dir) ){
-			
-			if(entry -> d_type == DT_REG){	
-				if(strstr(entry -> d_name, ".csv")){
+
+		while (entry = readdir(dir)) {
+
+			if (entry->d_type == DT_REG) {
+				if (strstr(entry->d_name, ".csv")) {
 					pid = fork();
 					counter++;
-					if(pid == 0){
-						
+					if (pid == 0) {
+
 						//check the csv file 
-													
+
 						//sort_csv(entry -> d_name, field_name, NULL, output_path); 
-							
-					_exit(1);
+
+						_exit(1);
 					}
-					else if(pid > 0){
+					else if (pid > 0) {
 						ppid = getpid();
-						fprintf(ptr, "parent %d generates child %d in path %s to process file: %s\n", ppid, pid, name, entry -> d_name);
-						fflush(ptr);
-						t_pid[counter -1] = pid;
+						filelist[counter - 1] = malloc(strlen(entry -> d_name) + 2);
+						strcpy(filelist[counter - 1], entry -> d_name);
+						//fprintf(ptr, "parent %d generates child %d in path %s to process file: %s\n", ppid, pid, name, entry->d_name);
+						//fflush(ptr);
+						t_pid[counter - 1] = pid;
 						continue;
 					}
-	
-					else{
-							//error case
-					}	
-	
-						
+
+					else {
+						//error case
+					}
+
+
 				}
 			}
-				
 
-			if (entry -> d_type == DT_DIR){
-				if(entry -> d_name[0] != '.'){
+
+			if (entry->d_type == DT_DIR) {
+				if (entry->d_name[0] != '.') {
 					pid = fork();
 					counter++;
 					t_pid[counter - 1] = pid;
-					if(pid != 0){
+					if (pid != 0) {
 						ppid = getpid();
-						fprintf(ptr, "parent %d generates child %d in path %s to process directory: %s\n", ppid, pid, name, entry -> d_name);
-						fflush(ptr);
-						t_pid[counter-1] = pid;
+						//fprintf(ptr, "parent %d generates child %d in path %s to process directory: %s\n", ppid, pid, name, entry->d_name);
+						//fflush(ptr);
+						filelist[counter - 1 ] = malloc(strlen(entry -> d_name) + 2);
+						strcpy(filelist[counter - 1], entry -> d_name); 
+						t_pid[counter - 1] = pid;
+						dir_index[counter -1] = 1;
 						continue;
 					}
-					if (pid == 0){
+					if (pid == 0) {
 						char file[1024];
-						strcpy(file, entry -> d_name);
+						strcpy(file, entry->d_name);
 						strcat(name, "/");
 						strcat(name, file);
 						//printf("output path is %s\n", name);
-						process_num = navigation(name, output_path, ptr);
+						indent ++;
+						process_num = navigation(name, output_path, ptr, indent, entry -> d_name);
 						//printf("path %s has process_num %d\n", output_path, process_num);
 
 						_exit(process_num);
+					
 					}
 				}
 			}
-		}		
+		}
 	}
 
-		
-	if( pid > 0){
+	int c_process_num = 0;
+	if (pid > 0) {
 		int i;
-		for (i = 0; i < counter; i++){
-			waitpid( t_pid[i], &status, 0);
+		
+		if(indent >= 1){
+			for (i = 0; i < 30* (indent-1) + 27 ; i ++){
+				fprintf(ptr, " ");
+			}
+			//fflush(ptr);
+			fprintf(ptr, "---");
+			fprintf(ptr, "|");
+			//fflush(ptr);
+		}		
+		indent = indent + 1;
+		fprintf(ptr, "%s: pid is %d --|\n",dir_name, getpid());
+		//fflush(ptr);	
+		for (i = 0; i < counter; i++) {
+			waitpid(t_pid[i], &status, 0);
 			fprintf(stdout, "%d,", t_pid[i]);
 			fflush(stdout);
-			process_num += WEXITSTATUS(status);
+			c_process_num = WEXITSTATUS(status);
+			int m;
+			if(dir_index[i] == 0){
+				for( m = 0; m < c_process_num; m++){
+					int j;
+					// get the indent
+					for( j = 0; j < 30 * (indent - 1) + 27; j ++)
+						fprintf(ptr, " ");
+					//fflush(ptr);
+					fprintf(ptr, "---");
+					fprintf(ptr, "|");
+					//fflush(ptr);
+					//leave a line for a child
+					if(m == 0){
+						fprintf(ptr, "%s: pid is %d\n", filelist[i], t_pid[i]);
+						free(filelist[i]);
+					}
+							
+					else	{fprintf(ptr, "\n");}	
+						//fflush(ptr);}			
+				}
+				
+				fprintf(ptr, "\n");
+			}
+			process_num += c_process_num;
 		}
-	
-	}	
+		fflush(ptr);
+	}
 	process_num = process_num + 1; //the parent process
-	pid_t c_pid = getpid();	
-	
-	if(c_pid != ini_pid){
+	pid_t c_pid = getpid();
+
+	if (c_pid != ini_pid) {
 		//printf("process number is %d\n", process_num);
 		return process_num;
 	}
-	
-	else{
+
+	else {
 		//printf("process number is %d\n", process_num);
 		return process_num;
 	}
-	
+
 	
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 	int status;
 	int i;
 	int c = 0;
 	int cflag = 0;
 	int oflag = 0;
 	int dflag = 0;
-
+	struct dirent *entry;
 	/*for(i = 1; i < argc; i++){
-		if(strcmp(argv[i], "-c") == 0){
-			c= i;
-		}
-		else if(strcmp(argv[i], "-d") == 0){
-			d = i;
-		}
-		else if(strcmp(argv[i], "-o") == 0){
-			o = i;
-		}
-		else{
-			continue;
-		}
+	if(strcmp(argv[i], "-c") == 0){
+	c= i;
+	}
+	else if(strcmp(argv[i], "-d") == 0){
+	d = i;
+	}
+	else if(strcmp(argv[i], "-o") == 0){
+	o = i;
+	}
+	else{
+	continue;
+	}
 	}*/
 	char input_path[1024];
 	char *output_path;
-	getcwd( input_path, 1024);		
-	while((c = getopt (argc, argv, "c: d: o:")) != -1){
+	getcwd(input_path, 1024);
+	while ((c = getopt(argc, argv, "c: d: o:")) != -1) {
 		switch (c)
 		{
 		case 'c':
@@ -165,7 +212,7 @@ int main(int argc, char** argv){
 			break;
 		case 'd':
 			dflag = 1;
-			if (optarg == NULL){
+			if (optarg == NULL) {
 				fprintf(stderr, "no start path\n");
 				exit(1);
 			}
@@ -173,7 +220,7 @@ int main(int argc, char** argv){
 			break;
 		case 'o':
 			oflag = 1;
-			if (optarg == NULL){
+			if (optarg == NULL) {
 				fprintf(stderr, "no output path\n");
 				exit(1);
 			}
@@ -184,32 +231,31 @@ int main(int argc, char** argv){
 			exit(1);
 			break;
 		}
-	}	 
-	if(cflag == 0 || argc < 3){
+	}
+	if (cflag == 0 || argc < 3) {
 		fprintf(stderr, "wrong argument\n");
 		exit(1);
 	}
 	process_num = 0;
-		// use default path	
+	// use default path	
 	FILE *ptr;
-	
-	ptr = fopen("pid_infor.txt", "w");	
-	
+	int indent = 0;
+	ptr = fopen("pid_infor.txt", "w");
+	char path[1024];
+	getcwd(path, 1024);
+	char *file_name = basename(path);
 	ini_pid = getpid();
 	printf("Initial PID: %d\n", ini_pid);
-	fprintf(stdout,"PIDS of all child processes:");
-	fflush(stdout);	
-	process_num = navigation(input_path, output_path, ptr);
+	fprintf(stdout, "PIDS of all child processes:");
+	fflush(stdout);
+	//printf("parent is %s\n", entry -> d_name);
+	process_num = navigation(input_path, output_path, ptr, indent, file_name);
 	fprintf(stdout, "\n");
 	fflush(stdout);
 	
-	printf("Total number of processes: %d\n", process_num);	
+	printf("Total number of processes: %d\n", process_num);
 	fflush(stdout);
-	
+	fclose(ptr);
 
 	return 0;
 }
-
-
-
-
